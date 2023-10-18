dca4cbe7ea60ffd137dd7af257566056bbbfeace
Richard Moser <85961946+richardmoser@users.noreply.github.com>
2023-10-18 10:54:54 -0500

    This is not the commit message you are looking for

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,7 @@
 //
 // Created by rj on 10/18/2023.
 //
+
+void print_splash(){
+
+}
--- lib.h
+++ lib.h
@@ -2,6 +2,8 @@
 // Created by rj on 10/18/2023.
 //
 
+void print_splash();
+
 #ifndef QUASH_LIB_H
 #define QUASH_LIB_H
 
